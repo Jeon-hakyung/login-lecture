6ef8f02947d35503fd06f3872d32c910d4c44a7a
jeon-hakyung <jhakyung12@gmail.com>
2024-07-11 22:46:14 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,9 +12,16 @@ function login() {
         psword: psword.value,
 
     };
-
     console.log(req);
+    console.log(JSON.stringify(req));
 
+fetch("/login", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(req), 
+})
     
 }
 
